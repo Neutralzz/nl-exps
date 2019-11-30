f3af0324596253f9b2fc7e1cdd120676a72df9de
Neutralzz <lizhongli1234@163.com>
2019-11-30 14:40:46 +0800

    mnli for tsne

---
+++ envs/setup_for_ai_studio.sh
@@ -0,0 +1,2 @@
+pip install torch==1.3.1+cu92 torchvision==0.4.2+cu92 -f https://download.pytorch.org/whl/torch_stable.html
+pip install transformers tensorboardX scikit-learn seqeval
---
+++ mnli_for_tsne/run_mnli.sh
@@ -0,0 +1,20 @@
+export GLUE_DIR=/home/aistudio/data/glue
+export TASK_NAME=MNLI
+export OUTPUT_DIR=/home/aistudio/work/output/test
+
+python ./examples/run_glue.py \
+    --model_type bert \
+    --model_name_or_path bert-base-uncased \
+    --task_name $TASK_NAME \
+    --config_name ./config.json \
+    --do_train \
+    --do_eval \
+    --do_lower_case \
+    --data_dir $GLUE_DIR/$TASK_NAME \
+    --max_seq_length 128 \
+    --per_gpu_eval_batch_size=16   \
+    --per_gpu_train_batch_size=32   \
+    --save_steps 0 \
+    --learning_rate 2e-5 \
+    --num_train_epochs 3.0 \
+    --output_dir $OUTPUT_DIR
